f636673c57742b7ef8b06225d08c49d59fd7a3cd
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-11-17 12:50:44 +0900

    게시물 상세보기 구현시작

--- src/com/kmg/exam/board/Main.java
+++ src/com/kmg/exam/board/Main.java
@@ -10,47 +10,48 @@ public class Main {
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     int articleLastId = 0;
-
-    while(true){
+    Article article = new Article();
+    while (true) {
       System.out.printf("명령)");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")){
+      if (cmd.equals("exit")) {
         System.out.println("== 프로그램 종료 ==");
         break;
-      }
-      else if(cmd.equals("/usr/article/write")){
+      } else if (cmd.equals("/usr/article/write")) {
         System.out.println("- 게시물 등록 -");
 
         System.out.printf("제목 : ");
-        String title = sc.nextLine();
+        article.title = sc.nextLine();
 
         System.out.printf("내용 : ");
-        String body = sc.nextLine();
+        article.body = sc.nextLine();
 
-        int id = articleLastId + 1;
-        articleLastId = id;
+        article.id = articleLastId + 1;
+        articleLastId = article.id;
 
-        Article article = new Article(id, title, body);
-        System.out.println("생성된 게시물 객체 : "+article);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n",id);
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+        continue;
+      } else if (cmd.equals("/usr/article/detail")) {
+        article.articleDetail();
         continue;
       }
-
     }
 
     sc.close();
   }
 }
+
 class Article {
   int id;
   String title;
   String body;
 
-  public Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
+  public void articleDetail() {
+    System.out.println("- 게시물 상세보기 -");
+    System.out.printf("번호 : %d\n제목 : %s\n내용 : %s\n", this.id, this.title, this.body);
   }
 
   public String toString() {
