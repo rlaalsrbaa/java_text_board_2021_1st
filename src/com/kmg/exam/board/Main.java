5a71bf0bf961d7863a18ed02152f2290d92e988d
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-11-17 10:26:24 +0900

    게시물 번호가 등록시마다 증가하도록 작업

--- src/com/kmg/exam/board/Main.java
+++ src/com/kmg/exam/board/Main.java
@@ -9,7 +9,8 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    int id = 0;
+    int articleLastId = 0;
+
     while(true){
       System.out.printf("명령)");
       String cmd = sc.nextLine();
@@ -24,7 +25,8 @@ public class Main {
         String title = sc.nextLine();
         ;        System.out.printf("내용 : ");
         String body = sc.nextLine();
-        id++;
+        int id = articleLastId + 1;
+        articleLastId = id;
         System.out.printf("%d번 게시물이 등록되었습니다.\n",id);
         continue;
       }
