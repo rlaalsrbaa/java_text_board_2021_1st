3100f1d4dc3daceb11de6dab8d2e02e7f3bb7b52
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-11-17 10:13:01 +0900

    exit 명령어 구현

--- src/com/kmg/exam/board/Main.java
+++ src/com/kmg/exam/board/Main.java
@@ -8,10 +8,16 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령)");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n",cmd);
-    System.out.println("== 프로그램 종료 ==");
+    while(true){
+      System.out.printf("명령)");
+      String cmd = sc.nextLine();
+      if(cmd.equals("exit")){
+        System.out.println("== 프로그램 종료 ==");
+        break;
+      }
+      System.out.printf("입력된 명령어 : %s\n",cmd);
+    }
+
     sc.close();
   }
 }
