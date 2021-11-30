2727be8b2551fa78f9d1808457961306b2d9708c
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-11-30 13:18:02 +0900

    Rq, Util 클래스를 만들어서 cmd 가공처리

--- src/com/jhs/exam/board/Main.java
+++ src/com/jhs/exam/board/Main.java
@@ -1,14 +1,13 @@
 package com.jhs.exam.board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
   static void makeTestData(List<Article> articles) {
     articles.add(new Article(1, "제목1", "내용1"));
     articles.add(new Article(2, "제목2", "내용2"));
     articles.add(new Article(3, "제목3", "내용3"));
+    articles.add(new Article(4, "제목4", "내용4"));
   }
 
   public static void main(String[] args) {
@@ -29,34 +28,42 @@ public class Main {
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
+      Rq rq = new Rq(cmd);
+      Map<String, String> params = rq.getParams();
 
-      if (cmd.equals("exit")) {
+      if (rq.getActionMethodName().equals("exit")) {
         break;
-      } else if (cmd.equals("/usr/article/list")) {
+      } else if (rq.getActionMethodName().equals("list")) {
         System.out.println("- 게시물 리스트 -");
         System.out.println("--------------------");
         System.out.println("번호 / 제목");
         System.out.println("--------------------");
+        if(params.get("orderBy").equals("idDesc")){
+          for (int i = articles.size() - 1; i >= 0; i--) {
+            Article article = articles.get(i);
+            System.out.printf("%d / %s\n", article.id, article.title);
 
-        for (int i = articles.size() - 1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf("%d / %s\n", article.id, article.title);
+          }
+        }else if (params.get("orderBy").equals("idAsc")){
+          for (Article article : articles){
+            System.out.printf("%d / %s\n", article.id, article.title);
+          }
         }
 
-      } else if (cmd.equals("/usr/article/detail")) {
+      } else if (rq.getActionMethodName().equals("detail")) {
 
-        if (articles.isEmpty()) {
+        if (Integer.parseInt(params.get("id")) > articles.size()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        Article article = articles.get(articles.size() - 1);
+        Article article = articles.get(Integer.parseInt(params.get("id")) - 1);
 
         System.out.println("- 게시물 상세내용 -");
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.title);
         System.out.printf("내용 : %s\n", article.body);
-      } else if (cmd.equals("/usr/article/write")) {
+      } else if (rq.getActionMethodName().equals("write")) {
         System.out.println("- 게시물 등록 -");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
@@ -95,5 +102,82 @@ class Article {
   @Override
   public String toString() {
     return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
+  }
+}
+
+class Rq {
+  private String url;
+  private String urlPath;
+  private Map<String, String> params;
+  private String controllerTypeCode;
+  private String controllerName;
+  private String actionMethodName;
+
+  Rq(String url) {
+    this.url = url;
+    urlPath = Util.getUrlPathFromUrl(this.url);
+    params = Util.getParamsFromUrl(this.url);
+    controllerTypeCode = Util.getControllerTypeCodeFromUrl(this.url);
+    controllerName = Util.getControllerName(this.url);
+    actionMethodName = Util.getActionMethodName(this.url);
+  }
+
+  public Map<String, String> getParams() {
+    return params;
+  }
+
+  public String getUrlPath() {
+    return urlPath;
+  }
+
+  public String getControllerTypeCode() {
+    return controllerTypeCode;
+  }
+
+  public String getControllerName() {
+    return controllerName;
+  }
+
+  public String getActionMethodName() {
+    return actionMethodName;
+  }
+}
+
+class Util {
+  static Map<String, String> getParamsFromUrl(String url) {
+
+    Map<String, String> params = new HashMap<>();
+    String[] urlBits = url.split("\\?", 2);
+
+    if (urlBits.length == 1) {
+      return params;
+    }
+
+    String queryStr = urlBits[1];
+    for (String bit : queryStr.split("&")) {
+      String[] bits = bit.split("=", 2);
+      if (bits.length == 1) {
+        continue;
+      }
+      params.put(bits[0], bits[1]);
+    }
+
+    return params;
+  }
+
+  static String getUrlPathFromUrl(String url) {
+    return url.split("\\?", 2)[0];
+  }
+
+  public static String getControllerTypeCodeFromUrl(String url) {
+    return url.split("\\?")[0].split("/")[1];
+  }
+
+  public static String getControllerName(String url) {
+    return url.split("\\?")[0].split("/")[2];
+  }
+
+  public static String getActionMethodName(String url) {
+    return url.split("\\?")[0].split("/")[3];
   }
 }
